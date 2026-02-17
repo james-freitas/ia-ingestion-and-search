9fd1817cf816f3bda9792fcad452bba5bc588d72
James Freitas <jamesfrj@yahoo.com.br>
2026-02-17 17:35:13 -0300

    search data using natural language

--- src/search.py
+++ src/search.py
@@ -1,3 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+
+load_dotenv()
+
+for k in ("OPENAI_API_KEY", "DATABASE_URL", "PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -25,5 +37,32 @@ PERGUNTA DO USUÁRIO:
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
+question = "Qual é o faturamento da empresa Alfa Energia Holding?"
+
 def search_prompt(question=None):
-    pass
+
+    embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"))
+
+    store = PGVector(
+      embeddings=embeddings,
+      collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+      connection=os.getenv("DATABASE_URL"),
+      use_jsonb=True
+    )
+
+    results = store.similarity_search_with_score(question, k=3)
+
+    for i, (doc, score) in enumerate(results, start=1):
+        print("="*50)
+        print(f"Resultado {i} (score: {score:.2f})")
+        print("="*50)
+
+        print("\nTexto:\n")
+        print(doc.page_content.strip())
+
+        print("\nMetadados:\n")
+        for k, v in doc.metadata.items():
+            print(f"{k}: {v}")
+
+if __name__ == "__main__":
+    search_prompt(question)
